ecd1127c7f77ce5de0262cb6b58b3b8240a16ae3
ひさお <hisayosidesu@icloud.com>
2020-07-21 16:49:51 +0900

    first commit

---
+++ app/src/main/java/h/j/photosharedservice/MainActivity.kt
@@ -0,0 +1,12 @@
+package h.j.photosharedservice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
