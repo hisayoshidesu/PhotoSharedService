10f57cab2a25e80a39327ede39b2fcb00cc098ce
ひさお <hisayosidesu@icloud.com>
2020-07-22 13:32:42 +0900

    fragmentの実装を作成

--- app/src/main/java/h/j/photosharedservice/MainActivity.kt
+++ app/src/main/java/h/j/photosharedservice/MainActivity.kt
@@ -2,11 +2,20 @@ package h.j.photosharedservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("Fragment", "onViewCreated")
         setContentView(R.layout.activity_main)
+
+        var fragmentManaer = supportFragmentManager
+        var fragmentTransaction = fragmentManaer.beginTransaction()
+        //backStackの設定 main画面からFragmentから画面をレイアウトする
+        fragmentTransaction.addToBackStack(null)
+        //fragmentTransactionにパラメータとして設定する
+        fragmentTransaction.add(R.id.container, SubClassFragmet()).commit()
     }
 }
---
+++ app/src/main/java/h/j/photosharedservice/SubClassFragmet.kt
@@ -0,0 +1,28 @@
+package h.j.photosharedservice
+
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class SubClassFragmet: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.main, container, false)
+    }
+
+}
+
