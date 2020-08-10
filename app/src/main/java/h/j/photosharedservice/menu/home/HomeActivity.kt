5448d5469c7128bf2accc854edc8ef9442405610
ひさお <hisayosidesu@icloud.com>
2020-08-10 09:57:56 +0900

    メソッドを追加

--- app/src/main/java/h/j/photosharedservice/menu/home/HomeActivity.kt
+++ app/src/main/java/h/j/photosharedservice/menu/home/HomeActivity.kt
@@ -1,4 +1,26 @@
 package h.j.photosharedservice.menu.home
 
-class homeActivity {
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+//    override fun onCreateView(
+//        parent: View?,
+//        name: String,
+//        context: Context,
+//        attrs: AttributeSet
+//    ): View? {
+//        return super.onCreateView(parent, name, context, attrs)
+//    }
+
 }
