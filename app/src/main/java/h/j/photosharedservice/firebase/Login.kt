a6f67dd88b77f91199cdc7520802612ad74f636c
ひさお <hisayosidesu@icloud.com>
2020-08-16 16:45:36 +0900

    新規作成

---
+++ app/src/main/java/h/j/photosharedservice/firebase/Login.kt
@@ -0,0 +1,11 @@
+package h.j.photosharedservice.firebase
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
