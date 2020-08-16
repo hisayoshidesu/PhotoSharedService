2eebb99344678078c4c206959bea05fd3e4c4163
ひさお <hisayosidesu@icloud.com>
2020-08-16 16:01:36 +0900

    メソッドの追記

--- app/src/main/java/h/j/photosharedservice/menu/other/AccountActivity.kt
+++ app/src/main/java/h/j/photosharedservice/menu/other/AccountActivity.kt
@@ -1,4 +1,28 @@
 package h.j.photosharedservice.menu.other
 
-class AccountActivity {
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class AccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onCreateView(
+        parent: View?,
+        name: String,
+        context: Context,
+        attrs: AttributeSet
+    ): View? {
+        return super.onCreateView(parent, name, context, attrs)
+    }
+
+
+
+
 }
--- app/src/main/java/h/j/photosharedservice/top/toolbarActivity.kt
+++ app/src/main/java/h/j/photosharedservice/top/toolbarActivity.kt
@@ -1,7 +1,15 @@
 package h.j.photosharedservice.top
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 /**
  * toolbarの処理記載
  */
-class toolbarActivity {
+class toolbarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
 }
