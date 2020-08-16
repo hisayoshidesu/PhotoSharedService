f81c998ab69963b833438d897f36193c2f33aeb7
ひさお <hisayosidesu@icloud.com>
2020-08-16 16:45:26 +0900

    新規作成

---
+++ app/src/main/java/h/j/photosharedservice/notisfaction/Notification.kt
@@ -0,0 +1,24 @@
+package h.j.photosharedservice.notisfaction
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class Notification : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        parent: View?,
+        name: String,
+        context: Context,
+        attrs: AttributeSet
+    ): View? {
+        return super.onCreateView(parent, name, context, attrs)
+    }
+}
