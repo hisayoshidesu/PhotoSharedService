6aee96718797752f92511d4e2631a6a5ef8234d1
ひさお <hisayosidesu@icloud.com>
2020-08-17 21:11:39 +0900

    fragmentで画面遷移

--- app/src/main/java/h/j/photosharedservice/MainActivity.kt
+++ app/src/main/java/h/j/photosharedservice/MainActivity.kt
@@ -3,6 +3,12 @@ package h.j.photosharedservice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import h.j.photosharedservice.menu.home.HomeFragment
+import h.j.photosharedservice.menu.like.LikeFragment
+import h.j.photosharedservice.menu.other.OtherFragment
+import h.j.photosharedservice.menu.schedule.ScheduleFragment
+import kotlinx.android.synthetic.main.main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,11 +17,60 @@ class MainActivity : AppCompatActivity() {
         Log.d("Fragment", "onViewCreated")
         setContentView(R.layout.activity_main)
 
-        var fragmentManaer = supportFragmentManager
-        var fragmentTransaction = fragmentManaer.beginTransaction()
-        //backStackの設定 main画面からFragmentから画面をレイアウトする
-        fragmentTransaction.addToBackStack(null)
-        //fragmentTransactionにパラメータとして設定する
-        fragmentTransaction.add(R.id.container, NavigationItemsFragmet()).commit()
+//        val navigationactivity =  NavigationActivity()
+//        navigationactivity.mOnNavigationItemSelectedListener
+
+//        var fragmentManaer = supportFragmentManager
+//        var fragmentTransaction = fragmentManaer.beginTransaction()
+//        //backStackの設定 main画面からFragmentから画面をレイアウトする
+//        fragmentTransaction.addToBackStack(null)
+//        //fragmentTransactionにパラメータとして設定する
+//        fragmentTransaction.add(R.id.container, NavigationItemsFragmet()).commit()
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+
+    val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                val fragmentManager = supportFragmentManager
+                var fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.addToBackStack(null)
+                fragmentManager.popBackStack()
+                fragmentTransaction.add(R.id.container_nav_home, HomeFragment()).commit()
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_schedule -> {
+                var fragmentManager = supportFragmentManager
+                var fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.addToBackStack(null)
+                fragmentManager.popBackStack()
+                fragmentTransaction.add(R.id.container_nav_schedule, LikeFragment()).commit()
+
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_favorite -> {
+                var fragmentManager = supportFragmentManager
+                var fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.addToBackStack(null)
+                fragmentManager.popBackStack()
+                fragmentTransaction.replace(R.id.container_favorite, ScheduleFragment()).commit()
+
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_other -> {
+                var fragmentManager = supportFragmentManager
+                var fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.addToBackStack(null)
+                fragmentManager.popBackStack()
+                fragmentTransaction.replace(R.id.container_other, OtherFragment()).commit()
+
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
