ae53614b22a263f88a440d73fcfebefb0522fbf4
ひさお <hisayosidesu@icloud.com>
2020-08-28 23:24:49 +0900

    一応、クラス作った

---
+++ app/src/main/java/h/j/photosharedservice/db/SQLiteActivity.kt
@@ -0,0 +1,18 @@
+package h.j.photosharedservice.db
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SQLiteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+}
