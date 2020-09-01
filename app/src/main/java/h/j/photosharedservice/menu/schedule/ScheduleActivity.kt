fd6e880a5bb65920d3a3c7d858ad9176bf6457d3
ひさお <hisayosidesu@icloud.com>
2020-09-01 23:05:06 +0900

    メソッドの定義した

--- app/src/main/java/h/j/photosharedservice/menu/schedule/ScheduleActivity.kt
+++ app/src/main/java/h/j/photosharedservice/menu/schedule/ScheduleActivity.kt
@@ -1,2 +1,30 @@
 package h.j.photosharedservice.menu.schedule
 
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
+class ScheduleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun getSupportActionBar(): ActionBar? {
+        return super.getSupportActionBar()
+    }
+
+    override fun checkUriPermission(uri: Uri?, pid: Int, uid: Int, modeFlags: Int): Int {
+        return super.checkUriPermission(uri, pid, uid, modeFlags)
+    }
+
+    override fun moveTaskToBack(nonRoot: Boolean): Boolean {
+        return super.moveTaskToBack(nonRoot)
+    }
+
+    
+
+    
+}
+
