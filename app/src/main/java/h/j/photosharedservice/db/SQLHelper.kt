0e20301f2cc5d2d32f45d877be14050f8e810231
ひさお <hisayosidesu@icloud.com>
2020-09-05 23:00:24 +0900

    クラス作成

---
+++ app/src/main/java/h/j/photosharedservice/db/SQLHelper.kt
@@ -0,0 +1,28 @@
+package h.j.photosharedservice.db
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class SQLHelper : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+    override fun openContextMenu(view: View?) {
+        super.openContextMenu(view)
+    }
+
+
+}
