ebf309b4b25e98c19ec74977d3459db3e72d81d5
ひさお <hisayosidesu@icloud.com>
2020-09-06 22:01:45 +0900

    修正

--- app/src/main/java/h/j/photosharedservice/http/HttpUrlConnection.kt
+++ app/src/main/java/h/j/photosharedservice/http/HttpUrlConnection.kt
@@ -1,9 +1,18 @@
 package h.j.photosharedservice.http
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 /**
  *
  * 内部内のhttp通信処理
  */
 
-class HttpUrlConnection {
+class HttpUrlConnection : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
 }
