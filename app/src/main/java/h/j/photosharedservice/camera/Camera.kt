39b7d15379ec477981a9ec58996107b51d879a7e
ひさお <hisayosidesu@icloud.com>
2020-09-13 08:18:38 +0900

    カメラクラス作成

---
+++ app/src/main/java/h/j/photosharedservice/camera/Camera.kt
@@ -0,0 +1,13 @@
+package h.j.photosharedservice.camera
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Camera : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
