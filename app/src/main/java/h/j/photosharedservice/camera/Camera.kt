8aea3df05f8096080ef8178a68145e97a978499d
ひさお <hisayosidesu@icloud.com>
2020-09-17 22:04:35 +0900

    メモの追記

--- app/src/main/java/h/j/photosharedservice/camera/Camera.kt
+++ app/src/main/java/h/j/photosharedservice/camera/Camera.kt
@@ -3,6 +3,10 @@ package h.j.photosharedservice.camera
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * 画像を押下後にカメラを起動し画像を差し替える
+ *
+ */
 class Camera : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
