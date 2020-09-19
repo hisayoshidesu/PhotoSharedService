d605714f2783ec0d6377a080617dd85ef5b45d10
ひさお <hisayosidesu@icloud.com>
2020-09-19 22:50:08 +0900

    ファイル読み込み用のファイル

---
+++ app/src/main/java/h/j/photosharedservice/db/File.kt
@@ -0,0 +1,22 @@
+package h.j.photosharedservice.db
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * 内部ストレージに保存
+ * https://developer.android.com/training/data-storage/files/internal?hl=ja
+ */
+
+class File : AppCompatActivity() {
+
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+}
