cbbd8887794e7a081505f6aa809258c8392769fa
ひさお <hisayosidesu@icloud.com>
2020-09-26 20:35:37 +0900

    クラス作成homefragment

--- app/src/main/java/h/j/photosharedservice/menu/home/HomeActivity.kt
+++ app/src/main/java/h/j/photosharedservice/menu/home/HomeActivity.kt
@@ -2,25 +2,32 @@ package h.j.photosharedservice.menu.home
 
 import android.content.Context
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.AttributeSet
-import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import h.j.photosharedservice.R
+import kotlinx.android.synthetic.main.home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fragmentManager = supportFragmentManager
+        var fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.addToBackStack(null)
+        fragmentManager.popBackStack()
+        fragmentTransaction.add(R.id.container_nav_home, HomeFragment()).commit()
+//        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+//        imageView2.setImageResource(R.drawable.sample_image)
+
+
     }
 
-
-//    override fun onCreateView(
-//        parent: View?,
-//        name: String,
-//        context: Context,
-//        attrs: AttributeSet
-//    ): View? {
-//        return super.onCreateView(parent, name, context, attrs)
+//    fun picture() {
+//        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+//        imageView2.setImageResource(R.drawable.sample_image)
+////        return imageView2
 //    }
+
 
 }
